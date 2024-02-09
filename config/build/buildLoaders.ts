0f531e3ac2239bee668fa938e02e24ed1362faf8
Dmitry Kumachev <dkumachev18@gmail.com>
2024-02-10 02:17:05 +0400

    ci: 'Установка react, react-dom. Настройка webpack для работы с react'

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,8 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-
+// без typescript нужно установить babel-loader
+// ts-loader умеет в .tsx
     const tsLoader = {
             test: /\.tsx?$/,
             use: 'ts-loader',

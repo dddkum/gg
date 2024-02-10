6e96fb97945d84c7a615422c5d714435be8d1976
Dmitry Kumachev <dkumachev18@gmail.com>
2024-02-10 16:36:43 +0400

    ft: 'Добавлен react-router с несколькими страницами'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Counter from "./components/Counter";
 import './index.scss'
+import {Route, Routes} from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/About/AboutPage";
+
 const App = () => {
     return (
         <div className="app">
-            HELLO
-            <Counter/>
+            <Routes>
+                <Route path='/' element={<MainPage/>}/>
+                <Route path='/about' element={<AboutPage/>}/>
+                <Route path='/counter' element={<Counter/>}/>
+            </Routes>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import {BrowserRouter, Outlet} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <App/>
+    <BrowserRouter>
+        <Outlet/>
+    </BrowserRouter>
 );

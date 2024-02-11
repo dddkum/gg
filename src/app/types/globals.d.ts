9495dca7c645482b82afaf19c34dd84a04831611
Dmitry Kumachev <dkumachev18@gmail.com>
2024-02-12 00:21:26 +0400

    ci: Добавлены лоадеры webpack для svg, png, jpg, jpeg файлов

--- src/app/types/globals.d.ts
+++ src/app/types/globals.d.ts
@@ -1,2 +1,12 @@
 declare module "*.module.css";
 declare module "*.module.scss";
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module "*.svg" {
+    import { SVGProps } from "react";
+    const content: React.FC<SVGProps<SVGSVGElement>>;
+    export default content;
+}
+

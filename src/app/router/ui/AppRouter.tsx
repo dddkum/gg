e18b8499bae447bc6b7b32c71e6c72f2ac2fec47
Dmitry Kumachev <dkumachev18@gmail.com>
2024-02-17 23:09:40 +0400

    stls: Добавлены стили для сайдбара и шаблона страниц

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -5,15 +5,17 @@ import {routesConfig} from "shared/config/routesConfig/routesConfig";
 const AppRouter = () => {
     return (
         <Suspense fallback={<div>LOADING</div>}>
-            <Routes>
-                {Object.values(routesConfig).map(({element, path}) => (
-                    <Route
-                        key={path}
-                        element={element}
-                        path={path}
-                    />
-                ))}
-            </Routes>
+            <div className="page-wrapper">
+                <Routes>
+                    {Object.values(routesConfig).map(({element, path}) => (
+                        <Route
+                            key={path}
+                            element={element}
+                            path={path}
+                        />
+                    ))}
+                </Routes>
+            </div>
         </Suspense>
     );
 };
